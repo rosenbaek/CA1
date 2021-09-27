d7cfa00175b6056150cc690f40d377f02327260d
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2021-09-27 15:43:39 +0200

    startcode adjusted

---
+++ src/main/java/entities/Hobby.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author christianrosenbaek
+ */
+@Entity
+public class Hobby implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(length = 50)
+    private String name;
+
+    private String wikiLink;
+    private String category;
+    private String type;
+
+    public Hobby() {
+    }
+
+    public Hobby(String name, String wikiLink, String category, String type) {
+        this.name = name;
+        this.wikiLink = wikiLink;
+        this.category = category;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWikiLink() {
+        return wikiLink;
+    }
+
+    public void setWikiLink(String wikiLink) {
+        this.wikiLink = wikiLink;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
