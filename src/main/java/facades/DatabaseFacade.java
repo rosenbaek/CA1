b618786079a0954375fe234488ee4ee17c8212cd
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2021-09-29 15:46:11 +0200

    merge

--- src/main/java/facades/DatabaseFacade.java
+++ src/main/java/facades/DatabaseFacade.java
@@ -6,8 +6,11 @@
 package facades;
 
 import entities.Person;
+import errorhandling.PersonNotFoundException;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -40,6 +43,50 @@ public class DatabaseFacade {
         return person;
     }
     
-   
+    public void deletePerson(int id) throws PersonNotFoundException{
+        EntityManager em = emf.createEntityManager();
+        Person person;
+        try {
+            em.getTransaction().begin();
+            person = em.find(Person.class, id);
+            if (person == null) {
+                throw new PersonNotFoundException("Could not delete, provided id does not exist!");
+            }
+            em.remove(person);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Person getPerson(int id) throws PersonNotFoundException {
+        EntityManager em = emf.createEntityManager();
+        Person person = em.find(Person.class, id);
+        if (person == null) {
+            throw new PersonNotFoundException("No person with provided id found");
+        } else {
+            return person;
+        }
+
+    }
+    
+    public List<Person> getAllPersons() {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p", Person.class);
+        List<Person> persons = query.getResultList();
+        return persons;
+    }
+    
+    public Person editPerson(Person person) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(person);
+            em.getTransaction().commit();
+            return person;
+        } finally {
+            em.close();
+        }
+    }
 
 }
